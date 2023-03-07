0919b065f3b788c7dffb4dfd9b00281745ccd0b6
Jdenyak <jdenyak@yandex.ru>
2023-03-07 16:47:59 +0700

    style commit

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,3 @@
-var order = true;
 var c = document.getElementById('board');
 var cells = [	c.rows[0].cells[0],	c.rows[1].cells[0],	c.rows[2].cells[0],
 
@@ -6,43 +5,7 @@ var cells = [	c.rows[0].cells[0],	c.rows[1].cells[0],	c.rows[2].cells[0],
 	
 		c.rows[0].cells[2],	c.rows[1].cells[2],	c.rows[2].cells[2]]
 
-document.getElementById('res').style.display = 'none';
-
-function gaPR() {
-
-	//restart if every cell in busy
-	var restart = true;
-	for (var i = 0; i < cells.length; i++) {
-		if (cells[i].innerHTML == '') {
-			restart = false;
-			break;
-		}
-	}
-
-	if (restart) {
-		document.getElementById('res').style.display = 'block';
-		setTimeout(function(){
-			location.reload();
-		}, 2500);
-	}
-
-	//choosin' random cell
-	rE = Math.floor(Math.random() * 9);
-	console.log(rE) //debuggin'
-
-	if (String(cells[rE].innerHTML) != '') {
-		gaPR();
-	} else {
-		cells[rE].innerHTML = 'o';
-	}
-	order = true;
-}
-
 document.getElementById('board').addEventListener("click", function(e) {
-	if (order) {
 		e.target.innerText = 'x';
-		order = false;
-		gaPR();
-	}
 })
 console.log(cells);
