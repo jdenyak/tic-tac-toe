a1aee8ed77d8c063340ae0fe43869b87767b7502
jdenyak <99114361+jdenyak@users.noreply.github.com>
2023-03-07 17:45:54 +0700

    Update logic.js

--- src/logic.js
+++ src/logic.js
@@ -6,6 +6,10 @@ var cells = [	c.rows[0].cells[0],	c.rows[1].cells[0],	c.rows[2].cells[0],
 		c.rows[0].cells[2],	c.rows[1].cells[2],	c.rows[2].cells[2]]
 
 document.getElementById('board').addEventListener("click", function(e) {
-		e.target.innerText = 'x';
-})
+		if (e.target.getElementId() == 'border' {
+		    return 0;
+		} else {
+			e.target.innerText = 'x';
+		}
+		})
 console.log(cells);
